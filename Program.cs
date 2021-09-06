9e62ddedf2b2db9c16231f9e08e69780bdf52ac1
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-06 20:58:47 -0300

    Adiciona a solução do exercício olá mundo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("Olá usuário, eu me chamo Júlia!");
+            Console.ResetColor();
+        }
+    }
+}
